0eb5a5eebd3140557fcd26e67e9fb5abcfd54abb
JewinJeong <50851841+JewinJeong@users.noreply.github.com>
2020-09-11 17:52:22 +0900

    Update Search.py

--- DSTest/Search.py
+++ DSTest/Search.py
@@ -1,6 +1,6 @@
 import pymysql
 
-conn = pymysql.connect(host = 'localhost', user = 'root', password= 'jj8575412', db = 'db_score')
+conn = pymysql.connect(host = 'localhost', user = 'root', password= '*******', db = 'db_score')
 curs = conn.cursor()
 
 sql = """select sno, midterm, final from score where midterm >= 20 and final >= 20
